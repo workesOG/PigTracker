49364f7be68979b916fa03f691b097e907b1422b
workes <bassdard2012@gmail.com>
2026-05-19 18:54:58 +0200

    Integrate javaFX into main with new view

--- app/src/main/java/pigtracker/Main.java
+++ app/src/main/java/pigtracker/Main.java
@@ -4,28 +4,33 @@
 package pigtracker;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        testJavaFX(stage);
-    }
+    public void start(Stage stage) throws Exception {
+        System.out.println(
+            Main.class.getResource("/views/main-view.fxml")
+        );
 
-    public static void main(String[] args) {
-        launch();
-    }
+        FXMLLoader loader = new FXMLLoader(
+                Main.class.getResource(
+                        "/views/main-view.fxml"
+                )
+                );
+                
 
-    private void testJavaFX(Stage stage) {
-        Button button = new Button("JavaFX works!");
-
-        Scene scene = new Scene(button, 400, 200);
+        Scene scene = new Scene(loader.load());
 
         stage.setTitle("PigTracker");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
