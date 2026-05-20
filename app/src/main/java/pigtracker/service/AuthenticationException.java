1921814ff5a26a0a7b65fc96b5b5c2ba54e319c1
NikJak <nikolajjak@pm.me>
2026-05-20 19:44:36 +0200

    Add AuthenticationException

---
+++ app/src/main/java/pigtracker/service/AuthenticationException.java
@@ -0,0 +1,9 @@
+// Af Nikolaj Jakobsen
+
+package pigtracker.service;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
