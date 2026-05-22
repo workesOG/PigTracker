0c47abfb48f3820799f82c12c876833379dceef7
NikJak <nikolajjak@pm.me>
2026-05-22 16:17:34 +0200

    Add Animal Model

---
+++ app/src/main/java/pigtracker/model/Visit.java
@@ -0,0 +1,22 @@
+// Af Nikolaj Jakobsen
+
+package pigtracker.model;
+
+import java.time.LocalDateTime;
+
+public record Visit(
+    int id,
+    int animalNumber,
+    String responder,
+    int location,
+    LocalDateTime visitTime,
+    int durationSec,
+    Integer weightG,
+    int feedIntakeG
+) {
+
+    // Returns a copy of this visit with the given id (used after insert).
+    public Visit withId(int newId) {
+        return new Visit(newId, animalNumber, responder, location, visitTime, durationSec, weightG, feedIntakeG);
+    }
+}
