20ced0822858db49eb86e2ff41293e6e7a86fd75
NikJak <nikolajjak@pm.me>
2026-05-22 17:29:15 +0200

    Add logout functionality to Main Controller

--- app/src/main/java/pigtracker/controller/MainController.java
+++ app/src/main/java/pigtracker/controller/MainController.java
@@ -1,10 +1,19 @@
 package pigtracker.controller;
 
 import javafx.fxml.FXML;
+import pigtracker.Main;
+import pigtracker.service.UserService;
 
 public class MainController {
     @FXML
     private void handleLogout() {
-        System.out.println("Test");
+        // Nikolaj Jakobsen
+        UserService.logout();
+
+        try {
+            Main.showLoginView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
