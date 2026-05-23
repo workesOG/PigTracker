9f897660c0f851f5a406a5292312d8d2dbccd92f
workes <bassdard2012@gmail.com>
2026-05-23 16:30:58 +0200

    Add animal data view

---
+++ app/src/main/java/pigtracker/controller/AnimalDataController.java
@@ -0,0 +1,53 @@
+package pigtracker.controller;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import pigtracker.controller.components.FilterChipContainerController;
+import pigtracker.controller.components.FilterChipController;
+import pigtracker.controller.components.SegmentedToggleController;
+import pigtracker.model.MetricOption;
+
+@SuppressWarnings("unused")
+public class AnimalDataController {
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private ChoiceBox<MetricOption> sortDropdown;
+
+    @FXML
+    private SegmentedToggleController sortSegmentedToggleController;
+
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private FilterChipContainerController filterChipContainerController;
+
+    private ArrayList<MetricOption> availableMetrics;
+
+    @FXML
+    public void initialize() {
+        sortSegmentedToggleController.setOptions("Asc.", "Desc.");
+    }
+
+    public void setMetrics(ArrayList<MetricOption> metrics) {
+        availableMetrics = metrics;
+
+        sortDropdown.getItems().add(null);
+        sortDropdown.getItems().addAll(metrics);
+
+        filterChipContainerController.setMetrics(metrics);
+    }
+}
---
+++ app/src/main/java/pigtracker/controller/DataController.java
@@ -0,0 +1,41 @@
+package pigtracker.controller;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import pigtracker.model.MetricOption;
+
+public class DataController {
+    @FXML
+    AnimalDataController animalDataController;
+
+    @FXML
+    public void initialize() {
+        loadAnimalMetrics();
+    }
+
+    private void loadAnimalMetrics() {
+        ArrayList<MetricOption> animalMetrics = new ArrayList<>();
+
+        animalMetrics.add(new MetricOption("Animal ID", MetricOption.MetricType.INTEGER,
+                new Boolean[] { true, true, true, true }));
+        animalMetrics.add(
+                new MetricOption("FCR", MetricOption.MetricType.DECIMAL, new Boolean[] { true, true, true, true }));
+        animalMetrics.add(new MetricOption("Start Weight", MetricOption.MetricType.DECIMAL,
+                new Boolean[] { true, true, true, true }));
+        animalMetrics.add(
+                new MetricOption("Weight", MetricOption.MetricType.DECIMAL, new Boolean[] { true, true, true, true }));
+        animalMetrics.add(new MetricOption("Weight gain", MetricOption.MetricType.DECIMAL,
+                new Boolean[] { true, true, true, true }));
+        animalMetrics.add(new MetricOption("Feed intake", MetricOption.MetricType.DECIMAL,
+                new Boolean[] { true, true, true, true }));
+        animalMetrics.add(new MetricOption("Days tested", MetricOption.MetricType.INTEGER,
+                new Boolean[] { true, true, true, true }));
+        animalMetrics.add(
+                new MetricOption("Start day", MetricOption.MetricType.DATE, new Boolean[] { true, true, true, true }));
+        animalMetrics.add(new MetricOption("Location", MetricOption.MetricType.INTEGER,
+                new Boolean[] { true, true, true, true }));
+
+        animalDataController.setMetrics(animalMetrics);
+    }
+}
